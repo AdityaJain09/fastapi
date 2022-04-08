47f74aa4f9053ebf52ff4a3263a7d0c10e3a6f59
AdityaJain09 <42073000+AdityaJain09@users.noreply.github.com>
2022-04-08 23:18:02 +0500

    Modified few files

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .routers import post, user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
 # generate tables
-models.Base.metadata.create_all(bind = engine)
+# models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
